922718c377a0c87276e54391242658e111054087
Sunave <antti.suniala@gmail.com>
2015-07-08 21:43:06 +0300

    Add UI fade in to Start Menu

---
+++ Assets/Scripts/PanelFade.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PanelFade : MonoBehaviour {
+
+	public float fadeInTime;
+
+	void Awake () {
+		GetComponent<Image>().color = Color.black;
+	}
+	
+	void Start () {
+		GetComponent<Image>().CrossFadeAlpha(0f, fadeInTime, false);
+	}
+
+	void Update () {
+		if (Time.timeSinceLevelLoad >= fadeInTime) {
+			gameObject.SetActive (false);
+		}
+	}
+
+}
