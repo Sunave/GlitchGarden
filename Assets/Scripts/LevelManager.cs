1738f5eff88d884c530730068415519ee8c42036
Sunave <antti.suniala@gmail.com>
2015-07-14 18:09:31 +0300

    Refactor LevelManager for negative number errors, add SetStartVolume so game reads correct volume level from the start

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -8,6 +8,8 @@ public class LevelManager : MonoBehaviour {
 	void Start () {
 		if (autoLoadNextLevelAfter > 0) {
 			Invoke ("LoadNextLevel", autoLoadNextLevelAfter);
+		} else if (autoLoadNextLevelAfter < 0) {
+			Debug.LogWarning("Level auto load disabled, you should use a positive number in seconds");
 		}
 	}
 
---
+++ Assets/Scripts/SetStartVolume.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetStartVolume : MonoBehaviour {
+
+	private MusicManager musicManager;
+	
+	void Start () {
+		musicManager = GameObject.FindObjectOfType<MusicManager>();
+		if (musicManager) {
+			musicManager.SetVolume(PlayerPrefsManager.GetMasterVolume());
+		} else {
+			Debug.LogWarning("MusicManager not found");
+		}
+	}
+
+}
