a96ab192524ccfc45fd8309a57ba9af1fa9c820e
Sunave <antti.suniala@gmail.com>
2015-07-14 23:09:10 +0300

    Add Health to Attackers and Defenders

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -2,18 +2,22 @@
 using System.Collections;
 
 [RequireComponent (typeof (Rigidbody2D))]
+[RequireComponent (typeof (Health))]
 public class Attacker : MonoBehaviour {
 
 	private float currentSpeed;
 	private GameObject currentTarget;
+	private Animator animator;
 	
 	void Start () {
-		Rigidbody2D rigidBody = gameObject.AddComponent<Rigidbody2D>();
-		rigidBody.isKinematic = true;
+		animator = GetComponent<Animator>();
 	}
 
 	void Update () {
 		transform.Translate (Vector3.left * currentSpeed * Time.deltaTime);
+		if (!currentTarget) {
+			animator.SetBool ("IsAttacking", false);
+		}
 	}
 
 	void OnTriggerEnter2D (Collider2D collider) {
@@ -26,12 +30,17 @@ public class Attacker : MonoBehaviour {
 
 	// Called from the animator at the time of actual blow
 	public void StrikeCurrentTarget (float damage) {
-		Debug.Log(name + " strikes for " + damage + " damage!");
+		if (currentTarget) {
+			Health health = currentTarget.GetComponent<Health>();
+			if (health) {
+				health.DealDamage(damage);
+			}
+		}
+
 	}
 
 	public void Attack (GameObject target) {
 		currentTarget = target;
-
 	}
 	
 }
--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent (typeof (Health))]
 public class Defender : MonoBehaviour {
 
 	// Use this for initialization
