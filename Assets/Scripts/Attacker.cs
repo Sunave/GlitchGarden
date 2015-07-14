333d0668f054e85e4e8e5bfd4345612546e7d6e7
Sunave <antti.suniala@gmail.com>
2015-07-14 20:38:08 +0300

    Add trigger events to Attackers and Defenders

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -6,10 +6,15 @@ public class Attacker : MonoBehaviour {
 	[Range (-1f, 1.5f)]	public float walkSpeed;
 	
 	void Start () {
-	
+		Rigidbody2D rigidBody = gameObject.AddComponent<Rigidbody2D>();
+		rigidBody.isKinematic = true;
 	}
 
 	void Update () {
 		transform.Translate (Vector3.left * walkSpeed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter2D (Collider2D collider) {
+		Debug.Log (name + " trigger entered by " + collider);
 	}
 }
