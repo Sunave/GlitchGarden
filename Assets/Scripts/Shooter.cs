85b070c1ca1adc91ff8a9b522a3f043dfc435371
Sunave <antti.suniala@gmail.com>
2015-07-16 21:06:50 +0300

    Add attack state detection to Defenders

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,10 +4,16 @@ using System.Collections;
 public class Shooter : MonoBehaviour {
 
 	public GameObject projectile;
+
 	private GameObject projectileParent;
 	private GameObject gun;
+	private Animator animator;
+	private AttackerSpawner thisRowSpawner;
 	
 	void Start () {
+
+		animator = GetComponent<Animator>();
+		FindThisRowSpawner();
 
 		// Find a Gun child
 		if (gameObject.transform.Find("Gun")) {
@@ -17,11 +23,42 @@ public class Shooter : MonoBehaviour {
 		// Find a parent to put projectiles into, or create one
 		projectileParent = GameObject.Find("Projectiles");
 		if (!projectileParent) projectileParent = new GameObject("Projectiles");
+
+	}
+
+
+	void Update () {
+		if (AttackerAheadInLane()) {
+			animator.SetBool( "isAttacking", true);
+		} else {
+			animator.SetBool( "isAttacking", false);
+		}
+	}
+
+	bool AttackerAheadInLane () {
+		if (thisRowSpawner.transform.childCount <= 0 ) return false;
+
+		foreach(Transform attacker in thisRowSpawner.transform) {
+			if (gameObject.transform.position.x < attacker.transform.position.x) return true;
+		}
+
+		return false;
+	}
+
+	// Look through all spawners, and set thisRowSpawner if found
+	void FindThisRowSpawner () {
+		AttackerSpawner[] spawners = GameObject.FindObjectsOfType<AttackerSpawner>();
+		foreach (AttackerSpawner spawner in spawners) {
+			if (transform.position.y == spawner.transform.position.y) {
+				thisRowSpawner = spawner;
+				return;
+			}
+		}
+		Debug.LogError (name + " can't find spawner in lane " + transform.position.y);
 	}
 
 	private void FireGun() {
 		GameObject newProjectile = Instantiate (projectile, gun.transform.position, Quaternion.identity) as GameObject;
 		newProjectile.transform.parent = projectileParent.transform;
-		//newProjectile.transform.position = gun.transform.position;
 	}
 }
