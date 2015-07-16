e53d5adda865c9d68326bf303c6022225f70d5d9
Sunave <antti.suniala@gmail.com>
2015-07-16 22:09:37 +0300

    Add display for stars, change trophy animation duration

---
+++ Assets/Scripts/StarDisplay.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent (typeof(Text))]
+public class StarDisplay : MonoBehaviour {
+
+	private static Text starText;
+	private static int totalStars;
+
+	void Start () {
+		starText = GetComponent<Text>();
+		UpdateView();
+	}
+
+	public void AddStars (int amount) {
+		totalStars += amount;
+		UpdateView();
+	}
+
+	public void UseStars (int amount) {
+		totalStars -= amount;
+		UpdateView();
+	}
+
+	private void UpdateView () {
+		starText.text = totalStars.ToString();
+	}
+}
