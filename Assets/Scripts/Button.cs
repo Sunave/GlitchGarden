bcd3e953e66f6960f93c14cfde2ecd06d2823aa5
Sunave <antti.suniala@gmail.com>
2015-07-17 13:15:48 +0300

    Add star cost for Buttons, tune star costs of Defenders

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Button : MonoBehaviour {
@@ -8,11 +9,16 @@ public class Button : MonoBehaviour {
 	public GameObject defenderPrefab;
 
 	private Button[] buttonArray;
+	private Text costText;
 
 	
 	void Start () {
 		buttonArray = GameObject.FindObjectsOfType<Button>();
 		GetComponent<SpriteRenderer>().color = Color.black;
+
+		costText = transform.GetComponentInChildren<Text>();
+		if (!costText) Debug.LogWarning ("Add a child with Text element for Button " + name);
+		costText.text = defenderPrefab.GetComponent<Defender>().starCost.ToString();
 	}
 
 	void OnMouseDown () {
