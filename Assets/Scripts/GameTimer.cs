e6eed74558c865a3e0d2f6b59d8eec25bce38ac8
Sunave <antti.suniala@gmail.com>
2015-07-17 11:23:41 +0300

    Add timer for level duration

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameTimer : MonoBehaviour {
+
+	public float levelPlayTimeSeconds = 30f;
+
+	private Slider slider;
+	private AudioSource audioSource;
+	private LevelManager levelManager;
+	private GameObject winLabel;
+	private bool endOfLevel = false;
+	
+	void Start () {
+		slider = GetComponent<Slider>();
+		audioSource = GetComponent<AudioSource>();
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
+
+		winLabel = GameObject.Find ("WinLabel");
+		if (!winLabel) Debug.LogWarning ("Please create UI Text child for LevelCanvas called 'WinLabel'");
+		winLabel.SetActive(false);
+	}
+
+	void Update () {
+		slider.value = 1 - (Time.timeSinceLevelLoad / levelPlayTimeSeconds);
+		if (Time.timeSinceLevelLoad >= levelPlayTimeSeconds && !endOfLevel) {
+			endOfLevel = true;
+			LevelSuccess();
+		}
+	}
+
+	void LevelSuccess() {
+		audioSource.Play();
+		winLabel.SetActive(true);
+		Invoke ("LoadNextLevel", audioSource.clip.length);
+	}
+
+	void LoadNextLevel () {
+		levelManager.LoadNextLevel();
+	}
+}
